cd5499b4434bd5f215af9d45dc2d8e3e63797c71
Tolu Emoruwa <105876577+EmoruwaTolu@users.noreply.github.com>
2023-09-20 10:15:00 -0400

    Update database.js
    Fixed MongodbURI issue. Password is no longer exposed

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ let dbConnection;
 
 module.exports = {
     connectToDB: (cb) => {
-        MongoClient.connect('mongodb+srv://emoruwatoluwanimi:AndreGray17@cluster0.rekr720.mongodb.net/PremierLeague365')
+        MongoClient.connect('mongodb+srv://emoruwatoluwanimi:<password>@cluster0.rekr720.mongodb.net/PremierLeague365')
         .then((client) => {
             dbConnection = client.db();
             return cb();
