84f93c530e9957c50ef2a15a12f70a4a7e37082d
Tolu <emoruwatoluwanimi@gmail.com>
2023-09-21 19:24:57 -0400

    Changed the webscraping code to get percentiles and accommodate players with multiple positions

--- webScrape.py
+++ webScrape.py
@@ -31,14 +31,9 @@ def teamRosterCreator(url):
     links =  team_roster.find("tbody")
     players = links.find_all("th")
 
-    # i = 0 
     playerLinks = []
     for playerUrls in players:
         playLink = "https://fbref.com" + str(playerUrls.a['href'])
-        # pos = players[i]
-        # p = pos.find_all("td")
-        # print(p[1].text)#left here in case you ever want to inclus players with multiple positions
-        # i = i + 1
         playerLinks.append(playLink)
 
     return(playerLinks)
@@ -61,42 +56,75 @@ def playerStandardStatsMaker(url, file):
     imagediv = s.find(id="meta")
     
     if checkingPosGK != None:
-        file.write(name.text + "GK ")
-        print(name.text + " GK")
+        file.write(name.text)
+        print(name.text)
     elif checkingPosMF != None:
-        file.write(name.text + "MF ")
-        print(name.text + " MF")
+        file.write(name.text)
+        print(name.text)
     elif checkingPosCB != None:
-        file.write(name.text + "CB ")
-        print(name.text + " CB")
+        file.write(name.text)
+        print(name.text)
     elif checkingPosFW != None:
-        file.write(name.text + "FW ")
-        print(name.text + " FW")
+        file.write(name.text)
+        print(name.text)
     elif checkingPosAM != None:
-        file.write(name.text + "AM ")
-        print(name.text + " AM")
+        file.write(name.text)
+        print(name.text)
     elif checkingPosFB != None:
-        file.write(name.text + "FB ")
-        print(name.text + " FB")
+        file.write(name.text)
+        print(name.text)
     else:
         file.write(name.text)
         print(name.text)
     
     data = s.find(id="all_scout_full")
-    tableData = data.find('table')
-    tData = tableData.find('tbody')
+
+    tables = data.find_all("div", class_="table_container")
+    utm = tables[0].find('table')
+    print(utm['id'][-2:])
+    tData = utm.find('tbody')
     tableRows = tData.find_all('tr')
+
+    if(len(tables) > 1):
+        utm2 = tables[1].find('table')
+        print(utm2['id'][-2:])
+        file.write(utm['id'][-2:] + "," + utm2['id'][-2:] + "      ")
+        tData2 = utm2.find('tbody')
+    else:
+        file.write(utm['id'][-2:]+ "      ")
+
+    percentile2=""
 
     if(checkingPosMF is None and checkingPosCB is None and checkingPosGK is None and checkingPosFW is None and checkingPosAM is None
     and checkingPosFB is None):
         print("we don't got that")
     else:
         print("he has it")
+        i = 0
         for tr in tableRows:
             time.sleep(3)
             if not tr.get('class'):
-                file.write(tr.td.text + "      ")
-                print(tr.th.text + "       " +tr.td.text)
+
+                raw_number = tr.td.text
+
+                percentile1_body  = tr.find('td', class_="left")
+
+                percentile1 = percentile1_body.div.text
+
+                if(len(tables) > 1):
+                    holder = tData2.find('th', text = tr.th.text)
+                    siblings = holder.findNextSiblings()
+                    percentile2 = siblings[1].div.text
+                    
+                if(percentile2 == ""):
+                    file.write(raw_number + "," + percentile1 + "      ")
+                    print(tr.th.text + "       " + raw_number + ", " + percentile1)
+
+                else:
+                    file.write(raw_number + "," + percentile1 + "," + percentile2 + "      ")
+                    print(tr.th.text + "       " + raw_number + ", " + percentile1 + ", " + percentile2)
+
+                i += 1
     
     if(imagediv != None):
         image = imagediv.find('img')
@@ -116,7 +144,7 @@ def specificSeason(url):
     links = uls.find_all('a')
 
     for a in links:
-        if(a.text == '2021-2022 Premier League'):
+        if(a.text == '2022-2023 Premier League'):
             final_link = a['href']
     
     print(final_link)
@@ -143,7 +171,7 @@ def specificSeason(url):
     
 #     file1.close()
 
-link = "https://fbref.com/en/squads/d07537b9/2022-2023/Brighton-and-Hove-Albion-Stats"
+link = "https://fbref.com/en/squads/822bd0ba/2022-2023/Liverpool-Stats"
 namingInfo = link.split("/")
 file1 = open(namingInfo[-1]+".txt", "w")
 teamInfo = teamRosterCreator(link)
