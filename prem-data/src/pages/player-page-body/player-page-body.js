67aa6f312df4e61c6af3ba1a408e1e15841d4702
Tolu <emoruwatoluwanimi@gmail.com>
2023-11-16 13:36:21 -0500

    Template started for bar chart display on Player Page

--- prem-data/src/pages/player-page-body/player-page-body.js
+++ prem-data/src/pages/player-page-body/player-page-body.js
@@ -4,10 +4,23 @@ import {useState} from 'react';
 
 function PlayerPageBody(props){
 
+    console.log(props.player.standard_stats.assists90[1])
 
     return(
         <div className='chart-body'>
             <PlayerPolarChart props={props}/>
+            <div className='numbers-container'>
+                <div className='player-statistics'>
+                    <div className="bar-container">
+                        <div className="bar" style={{ width: `${props.player.standard_stats.assists90[1]}%` }}>{props.player.standard_stats.assists90[0]}</div>
+                    </div>
+                </div>
+                <div>1</div>
+                <div>1</div>
+                <div>1</div>
+                <div>1</div>
+                <div>1</div>
+            </div>
         </div>
     )
 }
